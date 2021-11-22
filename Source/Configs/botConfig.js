d79b749944fbf089e1087dd3b1d92e04f8edbf51
diabloblood90 <94807077+diabloblood90@users.noreply.github.com>
2021-11-22 05:55:38 +0300

    Update botConfig.js

--- Source/Configs/botConfig.js
+++ Source/Configs/botConfig.js
@@ -1,6 +1,5 @@
 module.exports = {
   botToken: "", // Token
   botPrefix: "!", // Prefix
-  botPlaying: "Sleax <3 Code Share", // Oynuyor Kısmı
-  mongoDB_ConnectURL: "", // Mongodb bağlanma URL
+  botPlaying: "jerem yapımıdır", // Oynuyor Kısmı
 };
